4b1643ae79ec453b9665c78b5f5755a21d9ec720
Rey Wright <wright.rey@gmail.com>
2018-11-14 23:31:37 -0500

    removed console logs

--- src/index.js
+++ src/index.js
@@ -7,8 +7,6 @@ const problematicDateString = "2018-11-24T00:00:00+0000";
 const invalid = JSON.stringify(new Date(problematicDateString));
 const valid = format(problematicDateString, "dddd, MMMM D, YYYY");
 
-console.log(valid);
-
 const App = () => (
   <dl>
     <dt>Invalid string</dt>
